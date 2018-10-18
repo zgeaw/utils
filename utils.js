d050ef70bdc30479e96125c757d04869666b3b08
耿为刚 <gengweigang@gangquan360.com>
2018-10-18 09:19:18 +0800

    updata

--- utils.js
+++ utils.js
@@ -361,5 +361,49 @@ export default {
 	//元素滚动至固定位置
 	scrollElement(id, height = 0){
 		document.getElementsByClassName(id)[0].scrollTop = height
-	}
+	},
+  	//检测是否为空对象
+  	checkObj(obj) {
+  		let state = false
+  		if (JSON.stringify(obj) == '{}') {
+  			state = true
+  		}
+  		return state
+  	},
+  	//处理发布时间格式
+  	formatMsgTime (timespan) {
+  		let dateTime = new Date(timespan);
+  		let year = dateTime.getFullYear();
+  		let month = dateTime.getMonth() + 1;
+  		let day = dateTime.getDate();
+  		let hour = dateTime.getHours();
+  		let minute = dateTime.getMinutes();
+  		let second = dateTime.getSeconds();
+  		let now = new Date();
+  		let now_new = now.getTime();
+  		let newTimespan = dateTime.getTime();
+  		let milliseconds = 0;
+  		let timeSpanStr;
+
+  		milliseconds = now_new - newTimespan;
+
+  		if (milliseconds <= 1000 * 60 * 1) {
+  			timeSpanStr = '刚刚';
+  		}
+  		else if (1000 * 60 * 1 < milliseconds && milliseconds < 1000 * 60 * 60) {
+  			timeSpanStr = Math.round((milliseconds / (1000 * 60))) + '分钟前';
+  		}
+  		else if (1000 * 60 * 60 * 1 <= milliseconds && milliseconds <= 1000 * 60 * 60 * 24) {
+  			timeSpanStr = Math.round(milliseconds / (1000 * 60 * 60)) + '小时前';
+  		}
+  		else if (1000 * 60 * 60 * 24 < milliseconds && milliseconds <= 1000 * 60 * 60 * 24 * 15) {
+  			timeSpanStr = Math.round(milliseconds / (1000 * 60 * 60 * 24)) + '天前';
+  		}
+  		else if (milliseconds > 1000 * 60 * 60 * 24 * 15 && year == now.getFullYear()) {
+  			timeSpanStr = month + '-' + day + ' ' + hour + ':' + minute;
+  		} else {
+  			timeSpanStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute;
+  		}
+  		return timeSpanStr;
+  	}
 }
