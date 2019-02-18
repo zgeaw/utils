34ad542807fbce1b31434ccaa73d5cae8facb3c9
gengweigang <gengweigang@gangquan360.com>
2019-02-18 16:28:44 +0800

    update

--- utils.js
+++ utils.js
@@ -452,27 +452,27 @@ export default {
 			},0);
 		});
 	},
-  /*删除数组中的某一个对象
-  _arr:数组
-  _obj:需删除的对象
-  */
+	/*删除数组中的某一个对象
+	_arr:数组
+	_obj:需删除的对象
+	*/
 	removeAaary(_arr, _obj){
-    var length = _arr.length;
-    for (var i = 0; i < length; i++) {
-        if (_arr[i] == _obj) {
-            if (i == 0) {
-                _arr.shift(); //删除并返回数组的第一个元素
-                return _arr;
-            }
-            else if (i == length - 1) {
-                _arr.pop();  //删除并返回数组的最后一个元素
-                return _arr;
-            }
-            else {
-                _arr.splice(i, 1); //删除下标为i的元素
-                return _arr;
-            }
-        }
-    }
-  }
+		let length = _arr.length;
+		for (let i = 0; i < length; i++) {
+			if (_arr[i] == _obj) {
+				if (i == 0) {
+					_arr.shift(); //删除并返回数组的第一个元素
+					return _arr;
+				}
+				else if (i == length - 1) {
+					_arr.pop();  //删除并返回数组的最后一个元素
+					return _arr;
+				}
+				else {
+					_arr.splice(i, 1); //删除下标为i的元素
+					return _arr;
+				}
+			}
+		}
+	}
 }
